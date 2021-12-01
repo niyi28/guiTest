e94fe2b24b364e91a6507288d0eadd76c408f167
niyi28 <61952240+niyi28@users.noreply.github.com>
2021-12-01 09:02:20 +0100

    Fixed exception when numbers other than 1, 2, 3 chosen for game level and started on template for leaderboard

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,36 +4,45 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        /*https://www.youtube.com/watch?v=G4jMzEGMKfg
+        https://stackoverflow.com/questions/38907492/is-there-a-wysiwyg-option-for-intellij-idea-when-creating-a-gui-like-netbeans
+*/
+        // write your code here
         /*https://www.guru99.com/java-swing-gui.html*/
           /*  JFrame frame = new JFrame("Hangman Game");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(500,500);
             JButton button = new JButton("Press");
-            JButton button1 = new JButton("My Lord, My Lord");
             frame.getContentPane().add(button); // Adds Button to content pane of frame
             frame.getContentPane().add(button1); // Adds Button to content pane of frame
             frame.setVisible(true);*/
-
+        /*System.out.println("HOW TO PLAY \n ===========");
+        System.out.println(username + ", use your keyboard and guess each letter in the word. \nIf you guess right, you get two points and if you guess wrong, you loose a point. BEST OF LUCK !");
+        System.out.println("Press: P for PLAY");
+        System.out.println("Press: I for INSTRUCTIONS");
+        System.out.println("Press: C for CREDITS");
+        System.out.println("Press: Q for QUIT");*/
         JFrame frame = new JFrame("Hangman Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300,300);
-        JMenuBar mb = new JMenuBar();
-        JMenu file = new JMenu("FILE");
-        JMenu help = new JMenu("Help");
-        JMenu shiva = new JMenu("Shiva");
-        mb.add(file);
-        mb.add(help);
-        mb.add(shiva);
+        JMenuBar gameOverview = new JMenuBar();
+        JMenu play = new JMenu("Play");
+        JMenu leaderBoard = new JMenu("Leader Board");
+        JMenu instructions = new JMenu("Instructions");
+        JMenu credits = new JMenu("Credits");
+        JMenu quit = new JMenu("Quit");
+        gameOverview.add(play);
+        gameOverview.add(instructions);
+        gameOverview.add(leaderBoard);
+        gameOverview.add(credits);
+        gameOverview.add(quit);
 
-        JMenuItem underFile1 = new JMenuItem("Open");
-        JMenuItem underFile2 = new JMenuItem("Save as");
-        file.add(underFile1);
-        file.add(underFile2);
+
+
 
         JMenuItem underHelp = new JMenuItem("You can check the internet");
 
-        help.add(underHelp);
+        instructions.add(underHelp);
 
 
         JPanel panel = new JPanel(); // the panel is not visible in output
@@ -48,7 +57,7 @@ public class Main {
         panel.add(reset);
         JTextArea ta = new JTextArea();
         /*frame.setLayout(layout);*/
-        frame.getContentPane().add(BorderLayout.NORTH, mb); // Adds Button to content pane of frame
+        frame.getContentPane().add(BorderLayout.NORTH, gameOverview); // Adds Button to content pane of frame
         frame.getContentPane().add(BorderLayout.SOUTH, panel); // Adds Button to content pane of frame
         frame.getContentPane().add(BorderLayout.CENTER, ta); // Adds Button to content pane of frame
         // Adds Button to content pane of frame
